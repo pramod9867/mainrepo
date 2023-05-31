90a4fa9dca256b9277cffe65e3ab068e049b51d0
Pramod Patil <patil.pramod898@gmail.com>
2023-06-01 00:13:12 +0530

    master new code added

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.myapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+
+//master new code added
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)

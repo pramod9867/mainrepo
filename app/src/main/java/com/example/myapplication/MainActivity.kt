3d3f97e52636fb77ec281ae93f3f9a2069bdea14
Pramod Patil <patil.pramod898@gmail.com>
2023-06-01 00:12:06 +0530

    develop server mapping code added

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,8 +3,7 @@ package com.example.myapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-
-
+//develop server mapping code added
 //master new code added
 //new feature added
 class MainActivity : AppCompatActivity() {

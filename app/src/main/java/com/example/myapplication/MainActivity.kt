d8c1193c4153d13d1374d6cab3670f695c0b920f
Pramod Patil <patil.pramod898@gmail.com>
2023-06-01 00:12:06 +0530

    develop server mapping code added

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -6,6 +6,8 @@ import android.os.Bundle
 //develop server mapping code added
 //master new code added
 //new feature added
+
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)

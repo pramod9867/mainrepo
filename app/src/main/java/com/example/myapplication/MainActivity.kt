de1eae38964591aadfef4e306972ba0b424919e5
Pramod Patil <patil.pramod898@gmail.com>
2023-06-01 10:04:45 +0530

    Merge Master Into Current

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,10 +4,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 
+
 //master new code added
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun onSuccess(){
+
     }
 }
